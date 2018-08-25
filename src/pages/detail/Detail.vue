18421f46a2418a9b152a975778c78f1636aa3d16
He Bai <206678552@qq.com>
2018-08-26 03:13:39 +0800

    recursive detail-list

--- src/pages/detail/Detail.vue
+++ src/pages/detail/Detail.vue
@@ -2,17 +2,42 @@
   <div class="detail">
     <detail-banner></detail-banner>
     <detail-header></detail-header>
-    <div class="content"></div>
+    <div class="content">
+      <detail-list :detailList="detailList"></detail-list>
+    </div>
   </div>
 </template>
 <script>
 import DetailBanner from './components/Banner'
 import DetailHeader from './components/Header'
+import DetailList from './components/List'
 export default {
   name: 'Detail',
   components: {
     DetailBanner,
-    DetailHeader
+    DetailHeader,
+    DetailList
+  },
+  data () {
+    return {
+      detailList: [{
+        title: '促销票',
+        children: [{
+          title: '项目1',
+          children: [{
+            title: '第三列'
+          }, {
+            title: '第三列2'
+          }]
+        }, {
+          title: '项目2'
+        }]
+      }, {
+        title: '成人票'
+      }, {
+        title: '学生票'
+      }]
+    }
   }
 }
 </script>
---
+++ src/pages/detail/components/List.vue
@@ -0,0 +1,43 @@
+<template>
+  <div>
+    <div
+      class="list-item"
+      v-for="(item, index) of detailList"
+      :key="index"
+    >
+      <div class="item-title border-bottom">
+        <span class="item-title-icon"></span>
+        {{item.title}}
+      </div>
+      <div v-if="item.children">
+        <detail-list :detailList="item.children" class="item-children"></detail-list>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: 'DetailList',
+  props: {
+    detailList: Array
+  }
+}
+</script>
+<style lang="stylus" scoped>
+.item-title
+  line-height .8rem
+  font-size .32rem
+  padding 0 .2rem
+  .item-title-icon
+    position relative
+    top .06rem
+    left .06rem
+    display inline-block
+    width .36rem
+    height .36rem
+    margin-right .1rem
+    background-size .4rem .3rem
+    background-image url(http://s.qunaerzz.com/piao/image/touch/sight/detail.png) 0 -.45rem no-repeat
+.item-children
+  padding 0 .2rem
+</style>
