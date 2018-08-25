e12c0eb8097addd9f88ee92d6177528f5a614d2e
He Bai <206678552@qq.com>
2018-08-25 20:50:06 +0800

    search logic complete

--- src/pages/city/components/Search.vue
+++ src/pages/city/components/Search.vue
@@ -1,11 +1,64 @@
 <template>
-  <div class="search">
-    <input type="text" placeholder="输入城市名或拼音">
+  <div>
+    <div class="search">
+      <input v-model="keyword" type="text" placeholder="输入城市名或拼音">
+    </div>
+    <div class="search-content" ref="search" v-show="keyword">
+      <ul>
+        <li
+          class="border-bottom"
+          v-for="item of list"
+          :key="item.id"
+        >{{item.name}}</li>
+        <li class="border-bottom" v-show="hasNoData">没有找到匹配数据</li>
+      </ul>
+    </div>
   </div>
 </template>
 <script>
+import BScroll from 'better-scroll'
 export default {
-  name: 'CitySearch'
+  name: 'CitySearch',
+  props: {
+    cities: Object
+  },
+  data () {
+    return {
+      keyword: '',
+      timer: null,
+      list: []
+    }
+  },
+  computed: {
+    hasNoData () {
+      return !this.list.length
+    }
+  },
+  mounted () {
+    this.scroll = new BScroll(this.$refs.search)
+  },
+  watch: {
+    keyword () {
+      if (!this.keyword) {
+        this.list = []
+        return
+      }
+      if (this.timer) {
+        clearTimeout(this.timer)
+      }
+      this.timer = setTimeout(() => {
+        const result = []
+        for (let i in this.cities) {
+          this.cities[i].forEach((value) => {
+            if (value.spell.indexOf(this.keyword) > -1 || value.name.indexOf(this.keyword) > -1) {
+              result.push(value)
+            }
+          })
+        }
+        this.list = result
+      }, 100)
+    }
+  }
 }
 </script>
 <style lang="stylus" scoped>
@@ -23,4 +76,18 @@ export default {
     text-align center
     border-radius .06rem
     color #666
+.search-content
+  position absolute
+  top 1.58rem
+  left 0
+  right 0
+  overflow hidden
+  bottom 0
+  z-index 1
+  background #eee
+  li
+    line-height .62rem
+    padding-left .2rem
+    color #666
+    background #fff
 </style>
