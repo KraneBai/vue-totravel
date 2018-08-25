bd43e12510584dfb6e72a8c8d12ef0a5d7577110
He Bai <206678552@qq.com>
2018-08-25 20:13:08 +0800

    city list linkage with alphaebet

--- src/pages/city/components/Alphabet.vue
+++ src/pages/city/components/Alphabet.vue
@@ -1,6 +1,14 @@
 <template>
   <ul class="alphabet">
-    <li class="item" v-for="(item, key) of cities" :key="key">{{key}}</li>
+    <li class="item"
+      v-for="item of letters"
+      :key="item"
+      :ref="item"
+      @click="getLetter"
+      @touchstart="touchStart"
+      @touchmove="touchMove"
+      @touchend="touchEnd"
+    >{{item}}</li>
   </ul>
 </template>
 <script>
@@ -8,6 +16,39 @@ export default {
   name: 'CityAlphabet',
   props: {
     cities: Object
+  },
+  data () {
+    return {
+      touchStatus: false
+    }
+  },
+  computed: {
+    letters () {
+      const letters = []
+      for (var item in this.cities) {
+        letters.push(item)
+      }
+      return letters
+    }
+  },
+  methods: {
+    getLetter (e) {
+      this.$emit('change', e.target.innerText)
+    },
+    touchStart () {
+      this.touchStatus = true
+    },
+    touchEnd () {
+      this.touchStatus = false
+    },
+    touchMove (e) {
+      if (this.touchStatus) {
+        const startY = this.$refs['A'][0].offsetTop
+        const touchY = e.touches[0].clientY - 79
+        const index = Math.floor((touchY - startY) / 20)
+        this.$emit('change', this.letters[index])
+      }
+    }
   }
 }
 </script>
