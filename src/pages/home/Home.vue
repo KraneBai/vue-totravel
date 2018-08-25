6adfe6de4a72b355822b6bbb22912a72f5f008fa
He Bai <206678552@qq.com>
2018-08-25 13:46:57 +0800

    home page more specification

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,26 +1,26 @@
 <template>
   <div>
     <home-header></home-header>
-    <carousel></carousel>
-    <icons-area></icons-area>
-    <recommend></recommend>
-    <articles></articles>
+    <home-carousel></home-carousel>
+    <home-icons-area></home-icons-area>
+    <home-recommend></home-recommend>
+    <home-articles></home-articles>
   </div>
 </template>
 <script>
 import HomeHeader from './components/Header'
-import Carousel from './components/Carousel'
-import IconsArea from './components/IconsArea'
-import Recommend from './components/Recommend'
-import Articles from './components/Articles'
+import HomeCarousel from './components/Carousel'
+import HomeIconsArea from './components/IconsArea'
+import HomeRecommend from './components/Recommend'
+import HomeArticles from './components/Articles'
 export default {
   name: 'Home',
   components: {
     HomeHeader,
-    Carousel,
-    IconsArea,
-    Recommend,
-    Articles
+    HomeCarousel,
+    HomeIconsArea,
+    HomeRecommend,
+    HomeArticles
   }
 }
 </script>
